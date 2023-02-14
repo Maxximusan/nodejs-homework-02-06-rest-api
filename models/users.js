46a7e7147dfd2af3ab735bb961f881b5e26aebc6
Maxximusan <dragonstonecave@gmail.com>
2023-02-14 14:08:27 +0200

    5hw.1й
    Перенес Owner из users в contacts -- недосмотрел в 4й ветке

--- models/users.js
+++ models/users.js
@@ -26,11 +26,7 @@ const userSchema = Schema(
       type: String,
       default: null,
     },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
+   
   },
   { versionKey: false, timestamps: true }
 );
